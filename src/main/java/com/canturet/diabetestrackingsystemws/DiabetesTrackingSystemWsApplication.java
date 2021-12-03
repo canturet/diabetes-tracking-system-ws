7eeb285a15e405493a0ba3626987366d4072bc6c
canturet <turetcan@gmail.com>
2021-12-03 18:42:03 +0300

    project has been created

---
+++ src/main/java/com/canturet/diabetestrackingsystemws/DiabetesTrackingSystemWsApplication.java
@@ -0,0 +1,13 @@
+package com.canturet.diabetestrackingsystemws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetesTrackingSystemWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabetesTrackingSystemWsApplication.class, args);
+	}
+
+}
